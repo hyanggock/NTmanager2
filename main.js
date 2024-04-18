a205d2690584c228a661b9c36167f5e449aebd21
hyanggock <98065005+hyanggock@users.noreply.github.com>
2024-04-18 11:20:30 +0900

    Update main.js

--- main.js
+++ main.js
@@ -112,8 +112,7 @@ window.changeScene = function (scene) {
         for (var i = 1; i <= len; i++) {
           var tempinfo = snapData.child(i + '/info').val().toString();
           var tempstatus = snapData.child(i + '/notebookstatus').val().toString();
-          var tempnum = snapData.child(i).val().toString();
-          makecell(notebooknames[scene], tempnum, tempstatus, tempinfo);
+          makecell(notebooknames[scene], i, tempstatus, tempinfo);
         }
         if (before_scene != scene) {
           document.getElementById('cell_area').classList.remove("cell_area");
